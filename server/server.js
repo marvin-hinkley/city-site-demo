c801ff346d348036e3216f1686c101296b9ef9da
Marvin Hinkley <hinkley.marvin@gmail.com>
2018-08-12 12:38:08 -0700

    added basic server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express     = require('express'),
+    app         = express(),
+    http        = require('http'),
+    bodyParser  = require('body-parser'),
+    fs          = require('fs');
+
+app.use(express.static('build'));
+app.use('/node_modules', express.static('node_modules'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+http.createServer(app).listen(3000, '0.0.0.0');
+
+app.get('/', function (req, res) {
+    response.sendfile('../build/index.html');
+});
