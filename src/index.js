40e8717ef98b153bb5884f8bb1881dd3b5956c23
Marvin Hinkley <hinkley.marvin@gmail.com>
2018-08-12 15:37:40 -0700

    added facebook auth

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './index.scss';
+import './fb-login.js'
+
+var goldHill = goldHill || {};
+
+(function (gh) {
+  gh.checkLoginState = function() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  };
+})(goldHill);
