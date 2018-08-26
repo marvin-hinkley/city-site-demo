a9ee6ecc3145a5d38052d69229b3cef232a6a9fd
Marvin Hinkley <hinkley.marvin@gmail.com>
2018-08-26 09:25:58 -0700

    added unsubscribe and reminder

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ exports.subscribeFCMTopic = functions.https.onCall((data, context) => {
     throw new functions.https.HttpsError('invalid-argument', 'The function must be called with both a topic and a valid token');
   }
 
-  request('https://iid.googleapis.com/iid/v1/' + data.token + '/rel/topics/' + data.topic, (err, resp, body) => {
+  request.post('https://iid.googleapis.com/iid/v1/' + data.token + '/rel/topics/' + data.topic, (err, resp, body) => {
     if(err) {
       throw new functions.https.HttpsError('communication failure', err);
     }
@@ -18,3 +18,21 @@ exports.subscribeFCMTopic = functions.https.onCall((data, context) => {
     return {data: 'huzzah!'}
   });
 });
+
+exports.unsubscribeFCMTopic = functions.https.onCall((data, context) => {
+  if(typeof data.token == 'undefined' || typeof data.topic == 'undefined') {
+    throw new functions.https.HttpsError('invalid-argument', 'The function must be called with both a topic and a valid token');
+  }
+
+  request.delete('https://iid.googleapis.com/v1/web/iid/' + data.token + '/rel/topics/' + data.topic, (err, resp, body) => {
+    if(err) {
+      throw new functions.https.HttpsError('communication failure', err);
+    }
+
+    console.log('unregistered token with topic ' + data.topic);
+    return {data: 'huzzah!'}
+  });
+});
+
+// TODO: unsubscribeFCMTopic
+// TODO: updateFCMTopic
